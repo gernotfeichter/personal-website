ab2c9dcd26ffae721cc15d1a843f790f148de714
anon <absolutanon@hotmail.com>
2016-11-01 21:44:57 +0100

    initial commit

---
+++ js/myApp.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    var pages = ["home", "p_vs_np_proof" , "human_body_proportions", "intelligence", "spirituality", "about"];
+    $.each(pages,
+        function(index, value) {
+            var currentTemplateUrl = {templateUrl : "html/" + value + ".html"};
+            if (index == 0) {
+                $routeProvider.otherwise(currentTemplateUrl);
+            } else {
+                $routeProvider.when( "/" + value, currentTemplateUrl);
+            }
+
+        }
+    );
+});
