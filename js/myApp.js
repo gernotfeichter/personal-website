dc868f2e4b2844265ad3b2f475f9bc43d575ae1a
anon <absolutanon@hotmail.com>
2016-11-06 13:42:55 +0100

    rename draft page on spirituality to philosophy

--- js/myApp.js
+++ js/myApp.js
@@ -1,6 +1,6 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
-    var pages = ["home", "p_vs_np_proof" , "human_body_proportions", "intelligence", "spirituality", "about"];
+    var pages = ["home", "p_vs_np_proof" , "human_body_proportions", "intelligence", "philosophy", "about"];
     $.each(pages,
         function(index, value) {
             var currentTemplateUrl = {templateUrl : "html/" + value + ".html"};
