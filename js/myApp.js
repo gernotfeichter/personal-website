17e2062172293b21e4317f7cff1b6da586b66718
Gernot Feichter <absolutanon@hotmail.com>
2021-03-07 20:28:27 +0100

    add Creations

--- js/myApp.js
+++ js/myApp.js
@@ -1,6 +1,6 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
-    var pages = ["home", "p_vs_np_proof" , "interview", "human_body_proportions", "intelligence", "philosophy", "about"];
+    var pages = ["home", "p_vs_np_proof" , "interview", "human_body_proportions", "creations", "about"];
     $.each(pages,
         function(index, value) {
             var currentTemplateUrl = {templateUrl : "html/" + value + ".html"};
